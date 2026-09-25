56dfe425fdcb6d9c346b4f449c7ea5e836b8b943
mohammedalhar <mohameadalhaear100@gmail.com>
2026-09-25 03:36:05 +0300

    chore: remove obsolete local debug artifacts

--- check_cats2.py
+++
@@ -1,4 +0,0 @@
-﻿import sys
-content = open('index.html', 'r', encoding='utf-8').read()
-print('categories:', 'db.ref(\'categories\')' in content or 'db.ref("categories")' in content)
-print('products:', 'db.ref(\'products\')' in content or 'db.ref("products")' in content)
--- search.py
+++
@@ -1,4 +0,0 @@
-﻿with open('index.html', 'r', encoding='utf-8') as f:
-    for i, line in enumerate(f):
-        if 'giftForm' in line and 'submit' in line:
-            print(i+1, line[:100])
